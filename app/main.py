aaaf01c2242da6fd507c4585e0256ad590b76fea
DevOps Boy <99210504+RohitAswani@users.noreply.github.com>
2025-05-15 01:12:37 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from prometheus_client import start_http_server, Summary
+
 app = Flask(__name__)
 
-@app.route("/health")
-def health():
-    return "OK", 200
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
-if __name__ == "__main__":
+@REQUEST_TIME.time()
+@app.route('/')
+def index():
+    return 'Hello, from DevOps Boy!'
+
+if __name__ == '__main__':
+    start_http_server(5000)  # Expose metrics on port 5000
     app.run(host='0.0.0.0', port=5000)
-
